01d4d6c50bd28c6ffc89b951435d325a5e10d831
Nastya <anastasion0107@gmail.com>
2024-11-10 20:03:41 +0700

    create user model, register form

--- design/models.py
+++ design/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.validators import UnicodeUsernameValidator
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    username_validator = UnicodeUsernameValidator()
+
+    username = models.CharField(
+        _('username'),
+        max_length=150,
+        unique=True,
+        validators=[username_validator],
+        error_messages={
+            "unique": _('Пользователь с таким именем уже существует.')
+        },
+    )
+    first_name = models.CharField(_('Имя'), max_length=150, blank=True)
+    last_name = models.CharField(_('Фамилия'), max_length=150, blank=True)
+    email = models.EmailField(_('Email'), unique=True, blank=True)
+
+    REQUIRED_FIELDS = ['email', 'first_name', 'last_name']
+    USERNAME_FIELD = 'username'
+
+    def str(self):
+        return self.username
