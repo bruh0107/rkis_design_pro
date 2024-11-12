80b9c726bb588c8a1519e5150b60022505124a93
Nastya <anastasion0107@gmail.com>
2024-11-12 12:29:22 +0700

    create Application, Category model

--- design/models.py
+++ design/models.py
@@ -1,3 +1,5 @@
+from audioop import reverse
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
@@ -25,3 +27,35 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.email
+
+class Category(models.Model):
+    name = models.CharField(max_length=150, help_text="Название категории")
+
+    def __str__(self):
+        return self.name
+
+class Application(models.Model):
+    applicant = models.ForeignKey(CustomUser, on_delete=models.CASCADE, verbose_name="Пользователь", )
+    title = models.CharField(max_length=150, verbose_name="Название заявки")
+    description = models.TextField(max_length=150, verbose_name="Описание заявки")
+    image = models.FileField(upload_to=applicant, verbose_name="Загрузите фото заявки")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория заявки')
+
+    STATUS_CHOICES = [
+        ('N', "Новая"),
+        ('P', "Принято в работу"),
+        ('D', 'Выполнено')
+    ]
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES, default="N", verbose_name='Статус заявки')
+    date = models.DateTimeField(help_text="Дата создания заявки", auto_now_add=True)
+
+    def get_absolute_url(self):
+        return reverse('application-detail', args=[str(self.id)])
+
+    def display_category(self):
+        return self.category.name if self.category else "Категории нету"
+
+    def __str__(self):
+        return self.title
+
+    display_category.short_description = 'Category'
